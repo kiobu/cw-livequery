45189da8efb810f81586e4bd2cfb9691e13b9de9
kiobu <cdolson30@gmail.com>
2020-04-14 00:21:39 -0400

    Still in debugging phase. Finished majority of plugin, but HTTP requests continually fail. SteamHTTP has shitty ambiguous error messages.

---
+++ plugin/sv_hooks.lua
@@ -0,0 +1,36 @@
+function LQ:ClockworkInitialized()
+    MsgC(Color( 0, 255, 255 ), "[LiveQuery] LiveQuery is enabled on this server. POST requests will be sent to the LQ server.\n" )
+end;
+
+function LQ:PlayerCanSayOOC(player, text)
+    local requestbody = [[
+        {
+            "uid": "]]..LQ.Config.uid..[[",
+            "plyname": "]]..player:Name()..[[",
+            "svname": "]]..LQ.Config.svname..[[",
+            "msg": "]]..text..[[",
+            "time": ]]..os.time()..[[,
+        }
+    ]]
+
+    print("[LiveQuery] Creating POST request to dest: "..LQ.Config.server.." with request body: ")
+    print(requestbody)
+
+    local opts = {
+        parameters = "",
+        method = "post",
+        headers = {},
+        url = LQ.Config.server,
+        body = requestbody,
+        type = "application/json",
+        success = function(code, body, headers)
+            print("[LiveQuery] OK code "..code)
+        end,
+        failed = function(reason)
+            print("[LiveQuery] FAILED with reason: "..reason)
+        end
+    }
+
+    HTTP(opts)
+
+end;
